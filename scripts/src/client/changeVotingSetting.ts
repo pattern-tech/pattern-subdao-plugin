3aadbeee54f8a17e552ffe9246d6a02a2d61fa93
alikhabazian <khabaziana@gmail.com>
2023-11-28 17:11:38 +0330

    add all methods

--- scripts/src/client/changeVotingSetting.ts
+++ scripts/src/client/changeVotingSetting.ts
@@ -2,12 +2,30 @@
 import {Client, MultisigClient, TokenVotingClient} from "../lib/sdk";
 import {AllowedNetwork} from "../lib/constants";
 import {DaoDetails} from "@aragon/sdk-client";
-import {getVotingPluginAddress, iterateSteps, MULTISIG_PLUGIN_ID, TOKEN_VOTING_PLUGIN_ID} from "./installSubDAOPlugin";
-import {DAOABI, getABIEntry, MULTISIGABI, queryABIEntry, SUBDAOADMINABI} from '../lib/abis'
+import {
+    getVotingPluginAddress,
+    installSubDaoPlugin,
+    iterateSteps,
+    MULTISIG_PLUGIN_ID,
+    TOKEN_VOTING_PLUGIN_ID
+} from "./installSubDAOPlugin";
+import {
+    DAOABI,
+    getABIEntry,
+    GovernanceERC20ABI,
+    MULTISIGABI,
+    queryABIEntry,
+    SUBDAOADMINABI,
+    TOKENVOTINGABI
+} from '../lib/abis'
 import {getWallet, hexToBytes} from "../lib/helpers";
 import {ethers} from "ethers";
 import {DaoAction} from "@aragon/sdk-client-common";
 import {toBytes} from "../../../subdao-plugin/utils/helpers";
+import { parse, ArgumentConfig } from 'ts-command-line-args';
+
+const meta = import.meta as any;
+
 export class ChangeVotingSettingClient {
     readonly parentAddressOrEns:string;
     readonly childAddressOrEns:string;
@@ -24,6 +42,7 @@ export class ChangeVotingSettingClient {
     childVotingPluginContractAddress:any;
     childVotingPluginType:any;
     parentVotingClient:object;
+    votingToken:string;
 
 
     constructor(parentAddressOrEns:string,childAddressOrEns:string,subDaoPluginAddress:string,network:AllowedNetwork) {
@@ -33,11 +52,24 @@ export class ChangeVotingSettingClient {
         this.subDaoPluginAddress=subDaoPluginAddress;
         this.network=network;
         this.client = Client(network);
+        // console.log("constructor finished");
+
+    }
+
+    private async getVotingToken(){
+        const deployer=getWallet();
+        const Contract=this.connectContract(deployer,this.childVotingPluginContractAddress,TOKENVOTINGABI);
+        try {
+            const result = await Contract["getVotingToken"]();
+            return result;
+        } catch (error) {
+            console.error('Error calling contract function:', error);
+        }
 
     }
 
     async initial(){
-        //get parent detail
+        //get parent
         this.parentDaoDetails = await this.client.methods.getDao(this.parentAddressOrEns);
         if (!this.parentDaoDetails) throw new Error('DAO not found');
         //get child detail
@@ -61,8 +93,14 @@ export class ChangeVotingSettingClient {
         else if(parentVotingPluginType === MULTISIG_PLUGIN_ID){
             this.parentVotingClient = MultisigClient(this.network);
         }
+        if(childVotingPluginType===TOKEN_VOTING_PLUGIN_ID){
+            this.votingToken=await this.getVotingToken();
+        }
         this.initialized=true;
+        // console.log("initial finished");
     }
+
+
 
     private connectContract(deployer:ethers.Wallet,votingPluginAddress,contractABI){
         return new ethers.Contract(votingPluginAddress, contractABI, deployer);
@@ -79,9 +117,20 @@ export class ChangeVotingSettingClient {
         }
     }
 
-    private async checkIsAnyRepetetive(newapprovers:string[]){
-        for (let approver of newapprovers) {
-            let isMember=await this.isMemberMultisig(approver)
+    private async isMember(address:string,votingAbi:any){
+        const deployer=getWallet();
+        const Contract=this.connectContract(deployer,this.childVotingPluginContractAddress,votingAbi);
+        try {
+            const result = await Contract["isMember"](address);
+            return result;
+        } catch (error) {
+            console.error('Error calling contract function:', error);
+        }
+    }
+
+    private async checkIsAnyRepetetive(newApprovers:string[],votingAbi:any){
+        for (let approver of newApprovers) {
+            let isMember=await this.isMember(approver,votingAbi)
             if(isMember) throw new Error(`this new approver: ${approver} is already a member `);
         }
 
@@ -107,17 +156,15 @@ export class ChangeVotingSettingClient {
     private encodedFunctionData(contractABI,functionFragment:string,values:ReadonlyArray<any>){
         const contractABIFormated=getABIEntry(contractABI);
         const contractInterface = new ethers.utils.Interface(contractABIFormated);
-        console.log(values);
         return  contractInterface.encodeFunctionData(functionFragment, values);
     }
 
-    async multisigAddAddresses(newapprovers:string[]){
+    async multisigAddAddresses(newApprovers:string[]){
         if (!this.initialized) throw new Error('This instance has not initialized yet, first call initial() method');
-        if (newapprovers.length==0) throw new Error('There is not any approver to add');
-        await this.checkIsAnyRepetetive(newapprovers);
-        //porposal parent -> subdaoadminplugin -> childdao -> childvoting(multisig)
-        const encodedFunctionDataMultisig=this.encodedFunctionData(MULTISIGABI,'addAddresses',[newapprovers])
-        console.log(this.childVotingPluginContractAddress)
+        if (newApprovers.length==0) throw new Error('There is not any approver to add');
+        await this.checkIsAnyRepetetive(newApprovers,MULTISIGABI);
+        //porposal parent -> subdaoadminplugin -> childvoting(multisig)
+        const encodedFunctionDataMultisig=this.encodedFunctionData(MULTISIGABI,'addAddresses',[newApprovers])
         const updateVotingSettingDaoAction: DaoAction[] = [
             {
                 to: this.childVotingPluginContractAddress,
@@ -126,17 +173,7 @@ export class ChangeVotingSettingClient {
             },
         ];
 
-        // const encodedFunctionDataChildDAO=this.encodedFunctionData(DAOABI,'execute',['0x0000000000000000000000000000000000000000000000000000000000000001',updateVotingSettingDaoAction,0])
-        // console.log(encodedFunctionDataChildDAO)
-        // const executeChildDDaoAction: DaoAction[] = [
-        //     {
-        //         to: this.childContractAddress,
-        //         value: BigInt(0),
-        //         data: hexToBytes(encodedFunctionDataChildDAO),
-        //     },
-        // ];
         const encodedFunctionDataSubDAOAdmin=this.encodedFunctionData(SUBDAOADMINABI,'execute',[updateVotingSettingDaoAction])
-        console.log(encodedFunctionDataSubDAOAdmin)
         const executeSubDAOAdminDaoAction: DaoAction[] = [
             {
                 to: this.subDaoPluginAddress,
@@ -147,6 +184,100 @@ export class ChangeVotingSettingClient {
         //TODO
         const proposalMetadata = {
             title: 'Add new members',
+            summary: 'Add new member to the multisig',
+            description: `By installing this plugin, the plugin will grant the execution access of child da to parent`,
+            resources: [
+                {
+                    url: 'https://patterns.community',
+                    name: 'Pattern',
+                },
+            ],
+            media: {
+                header: 'https://assets-global.website-files.com/65410dc30116ce87ecbef5cd/654f75bc7db9aa282ca87e21_Logo%2Btext%20White.png',
+                logo: 'https://assets-global.website-files.com/65410dc30116ce87ecbef5cd/654f75bc7db9aa282ca87e21_Logo%2Btext%20White.png',
+            },
+        };
+        await this.sendProposal(proposalMetadata,executeSubDAOAdminDaoAction);
+
+    }
+
+
+    async checkAreAllMember(formerapprovers:string[],votingAbi:any){
+        for (let approver of formerapprovers) {
+            let isMember=await this.isMember(approver,votingAbi)
+            if(!isMember) throw new Error(`this former approver: ${approver} is already not a member `);
+        }
+    }
+    async multisigRemoveAddresses(formerapprovers:string[]){
+        if (!this.initialized) throw new Error('This instance has not initialized yet, first call initial() method');
+        if (formerapprovers.length==0) throw new Error('There is not any formerapprover to remove');
+        await this.checkAreAllMember(formerapprovers,MULTISIGABI);
+        //porposal parent -> subdaoadminplugin -> childvoting(multisig)
+        const encodedFunctionDataMultisig=this.encodedFunctionData(MULTISIGABI,'removeAddresses',[formerapprovers])
+        const updateVotingSettingDaoAction: DaoAction[] = [
+            {
+                to: this.childVotingPluginContractAddress,
+                value: BigInt(0),
+                data: hexToBytes(encodedFunctionDataMultisig),
+            },
+        ];
+
+        const encodedFunctionDataSubDAOAdmin=this.encodedFunctionData(SUBDAOADMINABI,'execute',[updateVotingSettingDaoAction])
+        const executeSubDAOAdminDaoAction: DaoAction[] = [
+            {
+                to: this.subDaoPluginAddress,
+                value: BigInt(0),
+                data: hexToBytes(encodedFunctionDataSubDAOAdmin),
+            },
+        ];
+        //TODO
+        const proposalMetadata = {
+            title: 'Remove former members',
+            summary: 'Remove some member of this multisig',
+            description: `By installing this plugin, the plugin will grant the execution access of child da to parent`,
+            resources: [
+                {
+                    url: 'https://patterns.community',
+                    name: 'Pattern',
+                },
+            ],
+            media: {
+                header: 'https://assets-global.website-files.com/65410dc30116ce87ecbef5cd/654f75bc7db9aa282ca87e21_Logo%2Btext%20White.png',
+                logo: 'https://assets-global.website-files.com/65410dc30116ce87ecbef5cd/654f75bc7db9aa282ca87e21_Logo%2Btext%20White.png',
+            },
+        };
+        await this.sendProposal(proposalMetadata,executeSubDAOAdminDaoAction);
+
+
+    }
+
+    async tokenVotingIncreaseAddressVotingPower(newApprovers:string[],amounts:number[]){
+        if (!this.initialized) throw new Error('This instance has not initialized yet, first call initial() method');
+        if (newApprovers.length==0) throw new Error('There is not any approver to add');
+        if (newApprovers.length!=amounts.length) throw new Error('Length of newApprovers and howMuch must be equal ');
+        // await this.checkIsAnyRepetetive(newApprovers,TOKENVOTINGABI);
+        // parent porposal -> subdao plugin -> Token address
+        const mintDaoAction: DaoAction[]=[]
+        for (let indexOfApprovers in newApprovers){
+            let encodedFunctionDataERC20=this.encodedFunctionData(GovernanceERC20ABI,'mint',[newApprovers[indexOfApprovers],amounts[indexOfApprovers]])
+            let daoActionToken:DaoAction={
+                to:this.votingToken,
+                value:BigInt(0),
+                data:hexToBytes(encodedFunctionDataERC20),
+            };
+            mintDaoAction.push(daoActionToken);
+        }
+        const encodedFunctionDataSubDAOAdmin=this.encodedFunctionData(SUBDAOADMINABI,'execute',[mintDaoAction])
+        const executeSubDAOAdminDaoAction: DaoAction[] = [
+            {
+                to: this.subDaoPluginAddress,
+                value: BigInt(0),
+                data: hexToBytes(encodedFunctionDataSubDAOAdmin),
+            },
+        ];
+        //TODO
+        const proposalMetadata = {
+            title: 'mint the token for ',
             summary: 'Granting parent DAO execution permission on child dao',
             description: `By installing this plugin, the plugin will grant the execution access of child da to parent`,
             resources: [
@@ -165,8 +296,93 @@ export class ChangeVotingSettingClient {
     }
 
 
-}
 
-const changeVotingClient=new ChangeVotingSettingClient('parentdaotest.dao.eth','childmultisig3.dao.eth','0xFf8353c4a56B39824bDd2C6Eea4d25bCA03Dd5Ed','goerli')
-await changeVotingClient.initial();
-await changeVotingClient.multisigAddAddresses(['0x612A6506e7cdD093598D876d19c9e231737E72Be'])
+
+}
+if (meta.main) {
+    interface MyArgs {
+        childDaoAddress: string;
+        parentDaoAddress: string;
+        network: string;
+        subDaoPluginAddress:string;
+        func:string;
+        otherArgs: string[];
+    }
+
+    const config: ArgumentConfig<MyArgs> = {
+        childDaoAddress: {
+            type: String,
+            alias: 'c',
+            description: 'The child dao address or ENS',
+        },
+        parentDaoAddress: {
+            type: String,
+            alias: 'p',
+            description: 'The parent dao address or ENS',
+        },
+        network: {
+            type: String,
+            alias: 'n',
+            description: 'The network, can be one of [mainnet, goerli, polygon, mumbai, base, baseGoerli, local]',
+        },
+        subDaoPluginAddress:{
+            type: String,
+            alias: 's',
+            description: 'The Subdao-plugin that installed on child dao address'
+        },
+        func:{
+            type: String,
+            alias: 'f',
+            description: 'The function that you want do on subdao'
+        },
+        otherArgs: {
+            type: String,
+            multiple: true,
+            defaultOption: true,
+        },
+    };
+    const args = parse<MyArgs>(config);
+
+    const childDAO = args.childDaoAddress;
+    const parentDAO = args.parentDaoAddress;
+    const network: AllowedNetwork = args.network as AllowedNetwork;
+    const subAdminPlugin:string =  args.subDaoPluginAddress;
+    const func:string=args.func;
+
+    const changeVotingClient = new ChangeVotingSettingClient(parentDAO, childDAO, subAdminPlugin, network);
+    await changeVotingClient.initial();
+    switch (func) {
+        case "multisigAddAddresses":
+            if (args.otherArgs.length!=1) throw new Error('inputs is not compatible');
+            let newaddresses: string[]=args.otherArgs[0].slice(1, -1).split(',');
+            await changeVotingClient.multisigAddAddresses(newaddresses)
+            break;
+        case "multisigRemoveAddresses":
+            if (args.otherArgs.length!=1) throw new Error('inputs is not compatible');
+            let formeraddresses: string[] = args.otherArgs[0].slice(1, -1).split(',');
+            await changeVotingClient.multisigRemoveAddresses(formeraddresses)
+            break;
+        case "tokenVotingIncreaseAddressVotingPower":
+            if (args.otherArgs.length!=2) throw new Error('inputs is not compatible');
+            let addresses: string[] = args.otherArgs[0].slice(1, -1).split(',');
+            let amount:number[]=args.otherArgs[1].slice(1, -1).split(',').map(Number);
+            await changeVotingClient.tokenVotingIncreaseAddressVotingPower(addresses,amount)
+            break;
+    }
+
+
+// const changeVotingClient=new ChangeVotingSettingClient('parentdaotest.dao.eth','childmultisig3.dao.eth','0xFf8353c4a56B39824bDd2C6Eea4d25bCA03Dd5Ed','goerli')
+
+
+// multisigAddAddresses
+// await changeVotingClient.multisigAddAddresses(['0x612A6506e7cdD093598D876d19c9e231737E72Be'])
+// bun changevotingsetting -c childmultisig3.dao.eth -p parentdaotest.dao.eth -n goerli -s 0xFf8353c4a56B39824bDd2C6Eea4d25bCA03Dd5Ed -f multisigAddAddresses '["0x612A6506e7cdD093598D876d19c9e231737E72Be"]'
+
+// multisigRemoveAddresses
+// await changeVotingClient.multisigRemoveAddresses(['0x612A6506e7cdD093598D876d19c9e231737E72Be']);
+// bun changevotingsetting -c childmultisig3.dao.eth -p parentdaotest.dao.eth -n goerli -s 0xFf8353c4a56B39824bDd2C6Eea4d25bCA03Dd5Ed -f multisigRemoveAddresses '["0x612A6506e7cdD093598D876d19c9e231737E72Be"]'
+
+// tokenVotingIncreaseAddressVotingPower
+// await changeVotingClient.tokenVotingIncreaseAddressVotingPower(['0x612A6506e7cdD093598D876d19c9e231737E72Be'],[1])//
+// bun changevotingsetting -c childdaotokenvoting.dao.eth -p parentdaotest.dao.eth -n goerli -s 0xFf8353c4a56B39824bDd2C6Eea4d25bCA03Dd5Ed -f tokenVotingIncreaseAddressVotingPower "['0x612A6506e7cdD093598D876d19c9e231737E72Be']" "[1]"
+}
